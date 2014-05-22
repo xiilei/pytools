1f23e63849207dec18a47c174061f9b88afff721
empty_ <xilei125@163.com>
2014-05-22 18:32:14 +0800

    add options

--- icmp.py
+++ icmp.py
@@ -3,11 +3,15 @@
 
 __author__ = 'xilei'
 
-import socket
-from struct import pack
+import os
+import sys
 import time
 import array
+import socket
+
+import getopt
 from threading import Thread
+from struct import pack, unpack, calcsize
 
 
 class SenderThread(Thread):
@@ -18,37 +22,57 @@ class SenderThread(Thread):
         self._total = total
         self._packet = packet
         self._ip = ip
-        self.count = 0
 
     def run(self):
         for i in range(self._total):
+            time.sleep(1)
             try:
                 self._sock.sendto(self._packet, (self._ip, 0))
-            except socket.timeout:
+            except socket.error as se:
+                print(se)
                 break
             finally:
-                self.count += 1
-            time.sleep(1)
+                #time.sleep(1)
+                pass
 
 
 def ping(ip, total=4, ipv6=False):
+    """
+    simple ping
+    """
     ipv6 = ipv6 and socket.has_ipv6 or False
     #sock.bind(('wlan0', socket.SOCK_RAW))
     family = ipv6 and socket.AF_INET6 or socket.AF_INET
-    sock = socket.socket(family, socket.SOCK_RAW, socket.getprotobyname('icmp'))
-    sock.settimeout(6)
-    sender = SenderThread(sock, icmppacket(), ip, total)
+    try:
+        sock = socket.socket(family, socket.SOCK_RAW, socket.getprotobyname('icmp'))
+        sock.settimeout(6)
+    except socket.error as ie:
+        print(ie)
+        return
+
+    cid = os.getpid() & 0xffffff
+
+    sender = SenderThread(sock, icmppacket(cid), ip, total)
     sender.start()
+
+    rc = 0
     while True:
         try:
-            if sender.count > total-1:
-                break
-            print(sock.recvfrom(1024))
-        except socket.error as e:
-            print(e)
+            recv_time = time.time()
+            recv_packet, addr = sock.recvfrom(1024)
+            head = recv_packet[20:28]
+            # i think so
+            ttl, = unpack('B', recv_packet[8:9])
+            type1, code, checksum1, packet_id, sequence = unpack("BBHHh", head)
+            if packet_id == cid:
+                send_time = unpack("d", recv_packet[28:28 + calcsize('d')])[0]
+                print("reply:%s time:%.2fms ttl=%s" % (addr[0], (recv_time-send_time-rc)*1000, ttl))
+        except socket.error as re:
+            print(re)
             break
         finally:
-            if not sender.is_alive():
+            rc += 1
+            if rc > total-1:
                 break
     sock.close()
 
@@ -65,16 +89,34 @@ def checksum(packet):
     return (~sum) & 0xffff
 
 
-def icmppacket(ipv6=False):
+def icmppacket(cid=98, ipv6=False):
     #type,code,Checksum,ID,Sequence
-    cid = 98
     sequence = 2
     head = pack('BBHHH', 8, 0, 0, cid, sequence)
     data = pack('d', time.time())
     csum = checksum(head+data)
     return pack('BBHHh', ipv6 and 128 or 8, 0, csum, cid, sequence)+data
 
-
-
+#todo KeyboardInterrupt
 if __name__ == "__main__":
-        ping('192.168.1.1', 4)
+    _ip = ''
+    _total = 4
+    _ipv6 = False
+    opts = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:t:i6", ["ip=", "total=", "ipv6"])
+    except getopt.GetoptError as e:
+        print(e)
+        sys.exit(0)
+    for opt, arg in opts:
+        if opt in ("-i", "--ip"):
+            _ip = arg.strip()
+        if opt in ("-t", "--total"):
+            _total = int(arg)
+        if opt in ("-i6", "--ipv6"):
+            _ipv6 = True
+
+    if len(_ip) >= 8:
+        ping(_ip, _total)
+    else:
+        print("invalid params,use like -i 192.168.1.1")
