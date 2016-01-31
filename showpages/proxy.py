1f858ae1a862ddc4ba062951c560ef674007af59
xiilei <xilei125@163.com>
2016-01-31 22:58:46 +0800

    add get_dist function

--- showpages/proxy.py
+++ showpages/proxy.py
@@ -6,12 +6,29 @@ A hackable http proxy
 """
 import logging
 
+from six.moves.urllib.parse import urlparse
 from gevent.pywsgi import WSGIServer,WSGIHandler
 
+def get_dist(environ):
+    http_host = environ.get('HTTP_HOST', '').lower()
+    http_host_partail = http_host.split(':')
+    if len(http_host_partail) == 2:
+        return http_host_partail[0], int(http_host_partail[1])
+    return http_host,80
+
+def is_visible_request():
+    '''
+    return http request is visible by user's bowser
+    '''
+    return False
+
+def proxy_request():
+    pass
 
 def application(environ,start_response):
     start_response("200 OK",[("Content-Type", "text/plain; charset=utf-8")])
-    return b'hello,world'
+    host,port = get_dist(environ)
+    return b'request to %s:%d' % (host,port)
 
 if __name__ == '__main__':
     WSGIServer(':8080', application).serve_forever()
